4575675e05835c073424a644fbc678702d5bd581
chen_Y12 <1061362603@qq.com>
2018-07-06 16:01:26 +0800

    轮播制作完成

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
-Vue.config.productionTip = false
-
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper/* { default global options } */)
+// Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  // props: {
+  //   list: Array
+  // },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      list: [{
+        id: '0001',
+        imgUrl: 'https://images3.c-ctrip.com/hotel/2018/05/byy_780_235.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://images3.c-ctrip.com/ad/bigbutton/promotion/201806/0605_sjb_780_235.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'https://images3.c-ctrip.com/ad/bigbutton/promotion/201806/0626_sxkh_780_235.jpg'
+      }
+
+      ]
+    }
+  }
+  // computed: {
+  //   showSwiper () {
+  //     return this.list.length
+  //   }
+  // }
+}
+</script>
+
+<style lang="stylus" scoped>
+  //样式进行穿透，只要wrapper下出现swiper那个属性，就使用该样式，而不受scoped的限制
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
